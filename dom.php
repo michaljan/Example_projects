32abacde607f483b484bc542ac68290b3b687854
michaljan <michal.janicki@live.co.uk>
2013-04-24 18:00:10 +0200

    Przykladowe tworzenie obiektu

---
+++ dom.php
@@ -0,0 +1,47 @@
+class sciany{
+              public $material = "cegla";
+
+}
+class dach{
+
+                public $rodzajDachu = "Metal";                
+}
+
+class dom{
+
+                public $material;
+
+                public $rodzajDachu;
+
+                public $okna=array();
+
+                function __construct(sciany $material,dach $rodzajDachu){
+
+                                $this -> material = $material;
+
+                                $this -> rodzajDachu= $rodzajDachu;
+                }
+
+                public function dodajOkna($okna){
+
+                                $this -> okna = $okna;
+
+                }
+
+}
+
+$rodzajDachu = new dach();
+
+$material = new sciany();
+
+$modelZofia = new dom($material,$rodzajDachu);
+
+$okna["Parter"] = "Kwadrat";
+
+$okna["Pietro"] = "Prostokat";
+
+$modelZofia -> dodajOkna($okna);
+
+echo "<pre>";
+
+var_dump($modelZofia);
