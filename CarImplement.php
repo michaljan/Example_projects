2670db859f44f1be2e468d377f9ff6b5885dc239
michaljan <michal.janicki@live.co.uk>
2013-05-06 10:56:52 +0200

    Update CarImplement.php

--- CarImplement.php
+++ CarImplement.php
@@ -6,7 +6,7 @@ interface Car{
 class BMW implements Car{
 	public $age=null;
 	public function steringWeelMaterial(){
-		return "Stering weel is made of leather";
+		return "</br> Stering weel is made of leather";
 	}
 	public function carBody()
 	{
@@ -22,9 +22,32 @@ class BMW implements Car{
 	}
 }
 
+class Toyota implements Car{
+	public $age=null;
+	public function steringWeelMaterial(){
+		return "</br> Stering weel is made of plastic";
+	}
+	public function carBody()
+	{
+		return "</br> this is combi bodywork";
+	}
+	public function carAge($age){
+		$this ->age = $age;
+		if (is_numeric($age)){
+			return "this car is ". $this->age . " yeras old"; 
+		}
+		else
+			echo "No age avaliable";
+	}
+}
 
 
 $BMW_363 = new BMW();
 echo "BMW 363 has ". $BMW_363->steringWeelMaterial();
 echo " and ". $BMW_363->carBody();
 echo $BMW_363->carAge(10);
+
+$Toyota_Supra = new BMW();
+echo "</br> Toyota Supra has ". $Toyota_Supra->steringWeelMaterial();
+echo " and ". $Toyota_Supra->carBody();
+echo $Toyota_Supra->carAge(10);
